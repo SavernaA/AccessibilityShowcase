2b69c7546f9f5b3c5ad029d5b3017da1a6737cab
Saverna Ahmad <saverna.ahmad@gmail.com>
2026-05-24 15:19:38 -0400

    Initial Commit

---
+++ AccessibilityShowcase/AccessibilityShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityShowcaseApp.swift
+//  AccessibilityShowcase
+//
+//  Created by Saverna on 5/24/26.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
